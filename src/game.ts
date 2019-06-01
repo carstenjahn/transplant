ec1ca832829fd84229df1d43fa7a6d34003a0a26
Aicke Schulz <aicke@gmx.net>
2019-06-02 00:45:56 +0200

    added light & fps counter

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,43 @@
 import * as THREE from 'three';
+import * as Stats from 'stats.js';
 import {ShipControl, ShipAndCamera} from  './shipcontrol';
 import {Asteroids} from  './asteroids';
 import {World} from  './world';
 
 export default class Game {
+    private asteroids = new Asteroids();
     private camera;
     private renderer;
     private scene;
     private shipControl = new ShipControl();
     private shipAndCamera = new ShipAndCamera();
-    private asteroids = new Asteroids();
+    private stats;
 
     constructor() {
         //console.log('the answer to life, the universe, and everything is: ', config.answerToLifeTheUniverseAndEverything);
 
         this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x000009 );
+
         this.shipAndCamera.addToScene(this.scene);
         this.asteroids.addToScene(this.scene);
         this.camera = this.shipAndCamera.getCamera();
 
+        const ambientLight = new THREE.AmbientLight( 0x222222 );
+        this.scene.add( ambientLight );
+
+        const directionalLight = new THREE.DirectionalLight( 0xffffcc, 1 );
+        directionalLight.position.set(0, 0, 0);
+        directionalLight.target = this.shipAndCamera.getShip();
+        this.scene.add( directionalLight );
+
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.body.appendChild( this.renderer.domElement );
+
+        this.stats = new Stats();
+        this.stats.showPanel( 0 ); // 0: fps, 1: ms, 2: mb, 3+: custom
+        document.body.appendChild( this.stats.dom );
 
         window.addEventListener('keydown', (event) => this.handleKeyDown(event), false);
         window.addEventListener('keyup', (event) => this.handleKeyUp(event), false);
@@ -31,7 +47,7 @@ export default class Game {
             this.renderer.setSize(window.innerWidth, window.innerHeight); 
         });
 
-        requestAnimationFrame( () => this.animate() );
+        requestAnimationFrame(this.animate.bind(this));
     }
 
 
@@ -72,7 +88,7 @@ export default class Game {
 
 
     animate() {
-        requestAnimationFrame( () => this.animate() );
+        this.stats.begin();
 
         this.shipControl.nextFrame();
         this.asteroids.nextFrame(this.shipAndCamera.getCamera());
@@ -83,6 +99,9 @@ export default class Game {
         }
 
         this.renderer.render( this.scene, this.camera );
-    }
 
+        this.stats.end();
+
+        requestAnimationFrame(this.animate.bind(this));
+    }
 }
